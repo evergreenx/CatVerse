f074124e90d617ddfbc8fe896a0c0b01f24a3eab
ido dickson evergreen <dicksonevergreenido@gmail.com>
2021-09-23 00:59:14 +0100

    adding single cat page and route

--- src/component/CardTopTen.tsx
+++ src/component/CardTopTen.tsx
@@ -1,21 +1,40 @@
 import React from "react";
 
+
 type cardToptenTypes = {
-  data: string[];
+  data: any;
 };
 const CardTopTen = ({ data }: cardToptenTypes) => {
+  console.log(data, "rgecr");
   return (
-    <div className="card_topten flex">
-      <div className="card_topten_image"></div>
 
-      <div className="card_topten_text">
-        <ol className="card_topten_name  font-semibold text-4xl">
-          <li></li>
+    <>
+    <div className="card_topten justify-center flex my-20 flex-col lg:flex-row items-center content-center ">
+      <div className="card_topten_image w-96 mx-auto self-center   ">
+        <img
+          src={data?.image?.url}
+          className="rounded-3xl w-64 h-72 object-cover "
+          alt="cat"
+
+         
+        />
+      </div>
+
+      <div className="card_topten_text w-full mt-12 ">
+        <ol className="card_topten_name  font-semibold text-4xl list-disc">
+          <li> {data?.name} </li>
         </ol>
 
-        <div className="card_topten__details font-medium text-lg"></div>
+        <div className="card_topten__description font-medium text-lg">
+
+          {data.description}
+        </div>
       </div>
     </div>
+
+
+
+</>
   );
 };
 
--- src/component/Top10.tsx
+++ src/component/Top10.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import axios from "axios";
-import {
-  useQuery,
-
-} from "react-query";
+import { useQuery } from "react-query";
 import LoaderBar from "../component/LoaderBar";
 
 import Header from "./Header";
+import CardTopTen from "./CardTopTen";
+import Footer from "./Footer";
 const Top10 = () => {
   const { data, isLoading } = useQuery("cats", () =>
     axios.get("https://api.thecatapi.com/v1/breeds")
@@ -20,12 +19,15 @@ const Top10 = () => {
   return (
     <>
       <Header />
-      <div className=" w-full h-full text-basic-text">
+      <div className=" w-full h-full text-basic-text container">
         <h2 className="font-bold text-4xl">Top 10 most searched breeds</h2>
 
-       
-
+        {data?.data.slice(0, 10).map((i: any) => {
+          return <CardTopTen data={i} />;
+        })}
       </div>
+
+      <Footer />
     </>
   );
 };
