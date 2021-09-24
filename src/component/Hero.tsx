70fc556782b97377f92d35b27095878b68fe494e
ido dickson evergreen <dicksonevergreenido@gmail.com>
2021-09-24 17:19:55 +0100

    removed unfinished feature

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -6,11 +6,11 @@ const Hero = () => {
     <div className="bg-black rounded-t-3xl lg:p-32  p-10 hero ">
       <img src={Logo2} alt="logo" className="w-72" />
 
-      <h3 className="text-white font-semibold text-2xl mb-12">
-        Get to know more about your <br /> cat breed
+      <h3 className="text-white font-semibold text-2xl mb-12 w-64">
+        Get to know more about your cat breed
       </h3>
 
-      <div className="flex items-center bg-white w-80 rounded-full">
+      {/* <div className="flex items-center bg-white w-80 rounded-full">
         <input
           type="search"
           className="placeholder-basic-text font-semibold  bg-white p-5 rounded-3xl outline-none"
@@ -19,7 +19,7 @@ const Hero = () => {
         <i className="fa fa-search text-basic-text bg-white rounded-3xl p-5">
           {" "}
         </i>
-      </div>
+      </div> */}
     </div>
   );
 };
